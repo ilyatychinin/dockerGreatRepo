82940042ee3f57b2232e08e647ff15fad5fba6ac
ilyatychininHome <tychinin.ilya0709@gmail.com>
2025-11-22 21:33:46 +0300

    Revert "Merge branch 'dev'"
    This reverts commit 1689e1c89b5ef404f37fbc1aeff6fa2696e73d04, reversing
    changes made to 411fcfa7aa645a1e7229ad8632f1cac64b40970d.
    
    test revert commit

--- start-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-echo "build container image"
-docker image build -t linuxacademy/weather-app:v2 .
-
-echo "docker container starting"
-docker container run -d --name weather-dev -p 8082:3001 --env PORT=3001 linuxacademy/weather-app:v2
